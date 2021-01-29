4146596231952362eedfdc24aaaadd2cf8155b7c
CoderYihaoWang <yesiamyihao@gmail.com>
2021-01-29 22:51:14 +1300

    Extract PopCount to a package

--- ch2/exercises/3/main.go
+++ ch2/exercises/3/main.go
@@ -2,24 +2,8 @@ package main
 
 import (
 	"fmt"
+	"popcount/popcount"
 )
-
-var pc [256]byte
-
-func init() {
-	for i := range pc {
-		pc[i] = pc[i/2] + byte(i&1)
-	}
-}
-
-func popcount(x uint64) int {
-	var count byte
-	for i := 0; i < 8; i++ {
-		count += pc[byte(x)]
-		x >>= 8
-	}
-	return int(count)
-}
 
 func main() {
 	nums := [64]uint64{}
@@ -27,6 +11,6 @@ func main() {
 		nums[i] = 0xffffffffffffffff << i >> i
 	}
 	for _, n := range nums {
-		fmt.Printf("%064b: %d\n", n, popcount(n))
+		fmt.Printf("%064b: %d\n", n, popcount.PopCount(n))
 	}
 }
---
+++ ch2/exercises/3/popcount/popcount.go
@@ -0,0 +1,19 @@
+package popcount
+
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+// PopCount calculates the number of set bits in a uint64
+func PopCount(x uint64) int {
+	var count byte
+	for i := 0; i < 8; i++ {
+		count += pc[byte(x)]
+		x >>= 8
+	}
+	return int(count)
+}
