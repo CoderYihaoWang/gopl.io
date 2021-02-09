91aa238f66e02beda7c3269881e53a475f5c8d3a
Yihao <48015192+CoderYihaoWang@users.noreply.github.com>
2021-02-09 17:19:58 +1300

    Done ex 3.7

---
+++ ch3/exercises/7/main.go
@@ -0,0 +1,98 @@
+// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+package main
+
+import (
+	"image"
+	"image/color"
+	"image/png"
+	"log"
+	"math/cmplx"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", makeHandler(mandelbrot))
+	http.HandleFunc("/mandelbrot", makeHandler(mandelbrot))
+	http.HandleFunc("/acos", makeHandler(acos))
+	http.HandleFunc("/sqrt", makeHandler(sqrt))
+	http.HandleFunc("/newton", makeHandler(newton))
+
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func makeImage(f func(complex128) color.Color) *image.RGBA {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax-ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			// Image point (px, py) represents complex value z.
+			img.Set(px, py, f(z))
+		}
+	}
+	return img
+}
+
+func makeHandler(f func(complex128) color.Color) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := png.Encode(w, makeImage(f))
+		if err != nil {
+			log.Fatalf("mandelbrot: %v", err)
+		}
+	}
+}
+
+func mandelbrot(z complex128) color.Color {
+	const iterations = 200
+	const contrast = 15
+
+	var v complex128
+	for n := uint8(0); n < iterations; n++ {
+		v = v*v + z
+		if cmplx.Abs(v) > 2 {
+			return color.Gray{255 - contrast*n}
+		}
+	}
+	return color.Black
+}
+
+//!-
+
+// Some other interesting functions:
+
+func acos(z complex128) color.Color {
+	v := cmplx.Acos(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{192, blue, red}
+}
+
+func sqrt(z complex128) color.Color {
+	v := cmplx.Sqrt(z)
+	blue := uint8(real(v)*128) + 127
+	red := uint8(imag(v)*128) + 127
+	return color.YCbCr{128, blue, red}
+}
+
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//    = z - (z^4 - 1) / (4 * z^3)
+//    = z - (z - 1/z^3) / 4
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return color.Gray{255 - contrast*i}
+		}
+	}
+	return color.RGBA{0xff, 0x55, 0x55, 0xff}
+}
